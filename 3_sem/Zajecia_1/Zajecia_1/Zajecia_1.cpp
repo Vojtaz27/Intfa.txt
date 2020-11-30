af9c4b8accb636e96da90b7f1e052986c80fd817
Vojtaz27 <62818302+Vojtaz27@users.noreply.github.com>
2020-11-30 16:44:45 +0100

    notatki do 1 zadania

--- 3_sem/Zajecia_1/Zajecia_1/Zajecia_1.cpp
+++ 3_sem/Zajecia_1/Zajecia_1/Zajecia_1.cpp
@@ -12,17 +12,17 @@ private:
 
 public:
 
-    Jacht(int poj, int pal){
+    Jacht(int poj, int pal){                                                    // nazwy zmiennych które są w funkcji są mało ważne są wykorzystywane tylko w tej funkcji
         cout << "jestem konstruktorem argumentowym"<< endl;
         pojemnosc = poj;
         paliwo = pal;
         //dystans = pojemnosc/ paliwo;
-        
+
     }
 
     Jacht(){
         cout << "jestem konstruktorem bez argumentowym"<< endl;
-        pojemnosc = rand() % 6000 + 4000;
+        pojemnosc = rand() % 6000 + 4000;                                       // rand musi mieć te nawiasy bo jest funkcją, wybieamy z ilu liczb ma losować i przedział dolny określamy wartością po plusie
         paliwo = rand() % 699 + 500;
         dystans = pojemnosc / paliwo;
         //dystans = 5;
@@ -67,9 +67,9 @@ public:
         return pojemnosc;
     }
 
-    Jacht operator+(Jacht& a) {
-        return Jacht((pojemnosc + pojemnosc / 2), paliwo + a.return_paliwo());
-    }
+    Jacht operator+(Jacht& a) {                                                 // ooo tu sie robi ciekawie dutaj w skrócie mówimy jak program ma się zachować kiedy dodajemy dwa jachty
+        return Jacht((pojemnosc + a.return_pojemnosc() / 2), paliwo + a.return_paliwo());  // w nawiasie pobieramy zmienną typu jacht i wrucamy ją do funkcji pod nazwą a
+    }                                                                                       // przy pomocy "a.return_pojemnosc()" wyciągamy konkretną zmienną z jachtu o nazwie "a"
 
 };
 
@@ -87,13 +87,13 @@ int main()
     cout << w1->wyswietl_dystans();
     jacht_3 = jacht_1 + jacht_2;
     jacht_3.wyswietl();
-    
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
